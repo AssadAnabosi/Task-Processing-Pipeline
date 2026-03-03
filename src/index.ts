4fbd8ab0e424216aa2a24868523c28dfc9ddf5e4
Assad Anabosi <assad@anabosi.com>
2026-03-04 00:51:20 +0200

    init(server):initial structure

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 8001;
+
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'Hello from the API!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
